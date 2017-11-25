2e3dd3b5c2ede69b5f9270c70a305654e7372e55
aevj34 <anthony.varr@gmail.com>
2017-11-25 14:05:52 -0500

    1

---
+++ School.Domain/Repostories/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using School.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Domain.Repostories
+{
+    public interface IStudentRepository
+    {
+         List<Student> GetStudents();
+
+    }
+}
---
+++ School.Web/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using School.Application;
+using School.Application.Dto;
+using School.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace School.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+          private SchoolApplicationService schoolApplicationService = new SchoolApplicationService() ;
+
+          public ActionResult Index()
+          {
+
+              List<Student> students = new List<Student>();
+              students = schoolApplicationService.GetStudents();
+
+              return View(students);
+          }
+          
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
